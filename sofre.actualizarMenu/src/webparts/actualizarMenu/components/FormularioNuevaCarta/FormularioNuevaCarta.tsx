fef043618d54891692afb170249ae4beb651a529
Carolina Muguruza <CMuguruza@sofrecom.com.ar>
2019-05-09 17:17:29 -0300

    Ultima

--- sofre.actualizarMenu/src/webparts/actualizarMenu/components/FormularioNuevaCarta/FormularioNuevaCarta.tsx
+++ sofre.actualizarMenu/src/webparts/actualizarMenu/components/FormularioNuevaCarta/FormularioNuevaCarta.tsx
@@ -10,86 +10,88 @@ import { DefaultButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 
 
 export default class FormularioNuevaCarta extends React.Component<any, any>{
-    render(){
+    render() {
 
         const textFieldIdNombre = getId('nombre');
         const textFieldIdPrecio = getId('precio');
         const { disabled, checked } = this.props;
 
-        return(
-            <div className= {styles.container}>
+        return (
+            <div className={styles.container}>
+                <div className={styles.row}></div>
 
-            <h3>Carta</h3>
-            <Label htmlFor={textFieldIdNombre}>Nombre:</Label>
-            <TextField className= {styles.ancho200} id={textFieldIdNombre} />
+                <h3>Carta</h3>
+                <Label htmlFor={textFieldIdNombre}>Nombre:</Label>
+                <TextField className={styles.ancho200} id={textFieldIdNombre} />
 
-            <Dropdown className= {styles.ancho200}
-                placeHolder='Seleccione las opciones'
-                label='Categoria:'
-                defaultSelectedKeys={ ['Prueba1', 'Prueba2', 'Prueba3'] }
-                multiSelect
-                options={
-                    [
-                    { key: 'Prueba1', text: 'Prueba1' },
-                    { key: 'Prueba2', text: 'Prueba2' },
-                    { key: 'Prueba3', text: 'Prueba3' }
-                    ]
-                }
-        />
-
-            <Toggle className= {styles.ancho200}
-                defaultChecked={ true }
-                label='Disponibilidad'
-                onAriaLabel='This toggle is checked. Press to uncheck.'
-                offAriaLabel='This toggle is unchecked. Press to check.'
-                onText='On'
-                offText='Off'
-                onFocus={ () => console.log('onFocus called') }
-                onBlur={ () => console.log('onBlur called') }
+                <Dropdown className={styles.ancho200}
+                    placeHolder='Seleccione las opciones'
+                    label='Categoria:'
+                    defaultSelectedKeys={['Prueba1', 'Prueba2', 'Prueba3']}
+                    multiSelect
+                    options={
+                        [
+                            { key: 'Prueba1', text: 'Prueba1' },
+                            { key: 'Prueba2', text: 'Prueba2' },
+                            { key: 'Prueba3', text: 'Prueba3' }
+                        ]
+                    }
                 />
 
-            <Label htmlFor={textFieldIdPrecio}>Precio:</Label>
-            <TextField className= {styles.ancho200} id={textFieldIdPrecio} />
+                <Toggle className={styles.ancho200}
+                    defaultChecked={true}
+                    label='Disponibilidad'
+                    onAriaLabel='This toggle is checked. Press to uncheck.'
+                    offAriaLabel='This toggle is unchecked. Press to check.'
+                    onText='On'
+                    offText='Off'
+                    onFocus={() => console.log('onFocus called')}
+                    onBlur={() => console.log('onBlur called')}
+                />
 
-            <Dropdown className= {styles.ancho200}
-                placeHolder='Seleccione las opciones'
-                label='Guarnición:'
-                defaultSelectedKeys={ ['Prueba1', 'Prueba2', 'Prueba3'] }
-                multiSelect
-                options={
-                    [
-                    { key: 'Prueba1', text: 'Prueba1' },
-                    { key: 'Prueba2', text: 'Prueba2' },
-                    { key: 'Prueba3', text: 'Prueba3' }
-                    ]
-                }
-        />
+                <Label htmlFor={textFieldIdPrecio}>Precio:</Label>
+                <TextField className={styles.ancho200} id={textFieldIdPrecio} />
+
+                <Dropdown className={styles.ancho200}
+                    placeHolder='Seleccione las opciones'
+                    label='Guarnición:'
+                    defaultSelectedKeys={['Prueba1', 'Prueba2', 'Prueba3']}
+                    multiSelect
+                    options={
+                        [
+                            { key: 'Prueba1', text: 'Prueba1' },
+                            { key: 'Prueba2', text: 'Prueba2' },
+                            { key: 'Prueba3', text: 'Prueba3' }
+                        ]
+                    }
+                />
 
                 <DefaultButton
-                    primary={ true }
+                    primary={true}
                     data-automation-id='test'
-                    disabled={ disabled }
-                    checked={ checked }
+                    disabled={disabled}
+                    checked={checked}
                     text='Aceptar'
-             />
+                />
                 <DefaultButton
-                    primary={ true }
+                    primary={true}
                     data-automation-id='test'
-                    disabled={ disabled }
-                    checked={ checked }
+                    disabled={disabled}
+                    checked={checked}
                     text='Cancelar'
-             />
+                />
 
 
 
-  </div>  
+            </div>
         );
-    
+
     }
-   
+
     private _log(str: string): () => void {
         return (): void => {
-          console.log(str);
+            console.log(str);
         }
-    
-}}
+
+    }
+}
